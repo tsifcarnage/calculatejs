e1555a230c3666b07d2f62030af368ed392f5e7b
tsifcarnage <59823010+tsifcarnage@users.noreply.github.com>
2025-01-05 21:11:27 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const display = document.getElementById("display");
+const img = document.getElementById("myhead");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const rizz = document.getElementById("rizz");
 
+open.addEventListener("click", () => {
+  img.src = "img/eyesopen.svg";
+});
+close.addEventListener("click", () => {
+  img.src = "img/eyesclose.svg";
+});
+rizz.addEventListener("click", () => {
+  if (img.src.includes("eyesopen.svg")) {
+    img.src = "img/eyesrizz.svg";
+  }
+});
 function appendToDisplay(input) {
-  display.value += input;//pour ajouter dans le donnee
+  display.value += input; //pour ajouter dans le donnee
 }
 
 function clearDisplay() {
